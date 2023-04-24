c2107dff1aa7933fa4a387d80a35122f199ee4e1
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-04-24 16:48:46 +0800

    update

--- src/Attributes/QueryAttributes.cs
+++ src/Attributes/QueryAttributes.cs
@@ -4,8 +4,9 @@ namespace DCFApixels.DragonECS
 {
     [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     public sealed class IncAttribute : Attribute { }
-
     [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     public sealed class ExcAttribute : Attribute { }
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public sealed class OptAttribute : Attribute { }
 }
 
