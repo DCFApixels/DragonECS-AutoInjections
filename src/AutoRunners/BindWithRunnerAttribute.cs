983dd41ba1535b9f82ebe54248631a55d54f5ef7
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-03-02 18:33:49 +0800

    fix

--- src/AutoRunners/BindWithRunnerAttribute.cs
+++ src/AutoRunners/BindWithRunnerAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 [AttributeUsage(AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
-sealed class BindWithRunnerAttribute : Attribute
+public sealed class BindWithRunnerAttribute : Attribute
 {
     public readonly Type runnerType;
     public BindWithRunnerAttribute(Type runnerType)
