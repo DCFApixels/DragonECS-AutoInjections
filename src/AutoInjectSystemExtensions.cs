73c10e98ca3ead591546e522043d7d5365b147ea
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-03-10 13:04:52 +0800

    add AutoInjectModule

--- src/AutoInjectSystemExtensions.cs
+++ src/AutoInjectSystemExtensions.cs
@@ -2,9 +2,18 @@
 {
     public static class AutoInjectSystemExtensions
     {
+        [MetaColor(MetaColor.DragonCyan)]
+        public class AutoInjectModule : IEcsModule
+        {
+            public bool isAgressiveInjection;
+            public void Import(EcsPipeline.Builder b)
+            {
+                b.AddUnique(new AutoInjectSystem(isAgressiveInjection));
+            }
+        }
         public static EcsPipeline.Builder AutoInject(this EcsPipeline.Builder self, bool isAgressiveInjection = false)
         {
-            self.Add(new AutoInjectSystem(isAgressiveInjection));
+            self.AddUnique(new AutoInjectSystem(isAgressiveInjection));
             return self;
         }
     }
